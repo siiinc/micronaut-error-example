b7d261ed1f36be19efc4cfdeb958f4f599075ed9
Jerry Rumbolt <jerry.rumbolt@maxar.com>
2022-02-16 23:56:25 -0330

    more clarity

--- src/main/java/com/example/MyController.java
+++ src/main/java/com/example/MyController.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
@@ -9,35 +8,26 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Error;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
-import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.validation.Validated;
 import jakarta.inject.Singleton;
-
-import java.util.List;
 
 @Singleton
 @Controller("/")
 @Validated
 public class MyController {
     @Get(uri = "/test/{id}")
-    public MutableHttpResponse<?> getTile(HttpRequest<?> request, @PathVariable("id") String id,
-                                          @Nullable @QueryValue("nodeid") String nodeid,
-                                          @Nullable @QueryValue("p") List<String> p,
-                                          @Nullable @QueryValue("gutter") Integer gutter)
-            throws Throwable
+    public MutableHttpResponse<?> getTile(HttpRequest<?> request, @PathVariable("id") String id)
     {
-
-        return HttpResponse.status(HttpStatus.OK);
-
+        return HttpResponse.ok("OK").status(HttpStatus.OK);
     }
 
     @Error( status = HttpStatus.NOT_FOUND, global = true)
     public HttpResponse<?> notFound(HttpRequest<?> request) {
-        return HttpResponse.serverError("");
+        return HttpResponse.serverError("Not found").status(HttpStatus.NOT_FOUND);
     }
 
     @Error( status = HttpStatus.NOT_ACCEPTABLE, global = true)
     public HttpResponse<?> notAcceptable(HttpRequest<?> request) {
-        return HttpResponse.serverError("");
+        return HttpResponse.serverError("Not acceptable").status(HttpStatus.NOT_ACCEPTABLE);
     }
 }
